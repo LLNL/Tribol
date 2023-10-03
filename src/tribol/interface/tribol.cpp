09002c746eff228d3fdbb0b61de5884cf27d6035
Steven Robert Wopschall <wopschall1@llnl.gov>
2023-10-03 10:42:00 -0700

    Moved setLoggingLevel warning to info.

--- src/tribol/interface/tribol.cpp
+++ src/tribol/interface/tribol.cpp
@@ -332,8 +332,8 @@ void setLoggingLevel( const int csId, const LoggingLevel log_level )
    CouplingScheme* couplingScheme  = csManager.getCoupling( csId );
    if ( !in_range(log_level, NUM_LOGGING_LEVELS) )
    {
-      SLIC_WARNING_ROOT("tribol::setLoggingLevel(): Logging level not an option; " << 
-                        "using 'error' level.");
+      SLIC_INFO_ROOT("tribol::setLoggingLevel(): Logging level not an option; " << 
+                     "using 'error' level.");
       couplingScheme->setLoggingLevel( tribol::ERROR );
    }
    else
