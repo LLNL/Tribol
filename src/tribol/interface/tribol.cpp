2d37877ac824cd53833d9af67bb63164ff282936
Steven Robert Wopschall <wopschall1@llnl.gov>
2024-02-08 16:38:05 -0800

    Moved mesh data allocation out of registerMesh and into the face data calculation routine.

--- src/tribol/interface/tribol.cpp
+++ src/tribol/interface/tribol.cpp
@@ -421,25 +421,7 @@ void registerMesh( integer meshId,
       mesh.sortSurfaceNodeIds();
    }
 
-   // allocate outward unit face normal arrays and centroid arrays and cell area array
    mesh.m_dim = dim;
-   mesh.deallocateArrays();
-
-   if (mesh.m_numCells > 0)
-   {
-      mesh.allocateArrays(dim);
-      initRealArray( mesh.m_nX,   mesh.m_numCells, 0. );
-      initRealArray( mesh.m_nY,   mesh.m_numCells, 0. );
-      initRealArray( mesh.m_cX,   mesh.m_numCells, 0. );
-      initRealArray( mesh.m_cY,   mesh.m_numCells, 0. );
-      initRealArray( mesh.m_area, mesh.m_numCells, 0. );
-   }
-
-   if (dim == 3 && mesh.m_numCells > 0)
-   {
-      initRealArray( mesh.m_nZ, mesh.m_numCells, 0. );
-      initRealArray( mesh.m_cZ, mesh.m_numCells, 0. );
-   }
 
 } // end of registerMesh()
 
