30a891d14577e3d108c56696233ce34254af3a81
Eric B. Chin <chin23@llnl.gov>
2024-09-26 17:05:03 -0700

    updated docs

--- src/tribol/common/ArrayTypes.hpp
+++ src/tribol/common/ArrayTypes.hpp
@@ -15,33 +15,57 @@
 namespace tribol
 {
 
-template <typename T, int DIM, typename ArrayType>
-using ArrayBaseT = axom::ArrayBase<T, DIM, ArrayType>;
-
+/**
+ * @brief Array allocated on the free store, i.e. the heap
+ */
 template <typename T, int DIM = 1, MemorySpace SPACE = MemorySpace::Dynamic>
 using ArrayT = axom::Array<T, DIM, toAxomMemorySpace<SPACE>::value>;
 
+/**
+ * @brief View of (i.e. non-owned) array allocated on the free store
+ */
 template <typename T, int DIM = 1, MemorySpace SPACE = MemorySpace::Dynamic>
 using ArrayViewT = axom::ArrayView<T, DIM, toAxomMemorySpace<SPACE>::value>;
 
+/**
+ * @brief Array allocated on automatic storage, i.e. the stack
+ */
 template <typename T, int N>
 using StackArrayT = axom::StackArray<T, N>;
 
+/**
+ * @brief Convenience definition for a one-dimensional array
+ */
 template <typename T, MemorySpace SPACE = MemorySpace::Dynamic>
 using Array1D = ArrayT<T, 1, SPACE>;
 
+/**
+ * @brief Convenience definition for a two-dimensional array
+ */
 template <typename T, MemorySpace SPACE = MemorySpace::Dynamic>
 using Array2D = ArrayT<T, 2, SPACE>;
 
+/**
+ * @brief Convenience definition for a one-dimensional array view
+ */
 template <typename T, MemorySpace SPACE = MemorySpace::Dynamic>
 using Array1DView = ArrayViewT<T, 1, SPACE>;
 
+/**
+ * @brief Convenience definition for a two-dimensional array view
+ */
 template <typename T, MemorySpace SPACE = MemorySpace::Dynamic>
 using Array2DView = ArrayViewT<T, 2, SPACE>;
 
+/**
+ * @brief Convenience definition for an array of array views
+ */
 template <typename T, MemorySpace SPACE = MemorySpace::Dynamic>
 using MultiArrayView = ArrayT<ArrayViewT<T, 1, SPACE>, 1, SPACE>;
 
+/**
+ * @brief Convenience definition for an array view of array views
+ */
 template <typename T, MemorySpace SPACE = MemorySpace::Dynamic>
 using MultiViewArrayView = ArrayViewT<const ArrayViewT<T, 1, SPACE>, 1, SPACE>;
 
